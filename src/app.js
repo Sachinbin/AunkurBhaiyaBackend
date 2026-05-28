86dc2f062b870d2f8821a0baf31045bd5230574b
Sachin Bind <sachinbind7977@gmail.com>
2026-05-28 15:18:10 +0530

    express setup in app file

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// Express package ko import kar rahe hain
+let express = require('express')
+
+// Express application ka instance create kar rahe hain
+let app = express()
+
+// app ko export kar rahe hain taki dusri files me use kar sake
+module.exports = app;
